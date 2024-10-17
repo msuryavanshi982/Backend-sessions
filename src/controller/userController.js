bd653983e1652f0a1ecec9a00ad68d50f9d1122a
Meenakshi <meenakshisuryavanshi@gmail.com>
2024-10-17 21:09:50 +0530

    update and delete APIs added

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,3 @@
-const { json } = require("express");
 const userModel = require("../model/userModel");
 
 const createUser = async (req, res) => {
@@ -16,10 +15,41 @@ const createUser = async (req, res) => {
 const getUser = async (req, res) => {
   try {
     const user = await userModel.find();
-    return res.status(201).send({ status: true, message: user });
+    console.log({ mind: user });
+    return res.status(200).send({ status: true, message: user });
   } catch (err) {
     res.status(500).send({ status: false, message: err });
   }
 };
 
-module.exports = { createUser, getUser };
+const updateUser = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const updateData = req.body;
+    const user = await userModel.findByIdAndUpdate(userId, updateData, {
+      new: true,
+    });
+    return res.status(200).send({
+      status: true,
+      message: "User data updated successfully",
+      data: user,
+    });
+  } catch (err) {
+    res.status(500).send({ status: false, message: err });
+  }
+};
+const deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const user = await userModel.findByIdAndDelete(userId);
+    return res.status(200).send({
+      status: true,
+      message: "User data deleted successfully",
+      data: user,
+    });
+  } catch (err) {
+    res.status(500).send({ status: false, message: err });
+  }
+};
+
+module.exports = { createUser, getUser, updateUser, deleteUser };
--- src/router/route.js
+++ src/router/route.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, getUser } = require("../controller/userController");
+const {
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require("../controller/userController");
 
 router.post("/user", createUser);
-router.get("/getuser", getUser);
+router.get("/get", getUser);
+router.put("/update/:userId", updateUser);
+router.delete("/delete/:userId", deleteUser);
 
 module.exports = router;
